6c51954e314e2381c8a9f429226918735c4f1bbf
Vict0r <viton4ik@gmail.com>
2022-12-15 15:50:32 +0100

    rev.02

--- main.py
+++ main.py
@@ -72,12 +72,26 @@ year_months = { 1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6
 year = 2023
 previous_year = 2022
 
-
 listOfHolidays = []
 listOfHolidays_date = []
-for ptr in holidays.RS(years=year).items():
+for ptr in holidays.RS(years=year).items(): # https://pythonpip.ru/osnovy/modul-holidays-python
+    # print(ptr)
+    # print(ptr[0])
     listOfHolidays.append((ptr[0], ptr[1]))
     listOfHolidays_date.append(ptr[0])
+# print(listOfHolidays[0][1])
+# print(listOfHolidays_date)
+
+# if datetime(2023, 1, 1).date() in listOfHolidays_date:
+#     print('dddqegwrgrg')
+
+# for i in listOfHolidays:
+    # print(i[0])
+    # print(datetime(2023, 1, 1).date())
+    # if datetime(2023, 1, 1).date() == i[0]:
+    #     print('dddd')
+
+
 
 # initializing the start and end date
 start_date = datetime(year,1,1)
@@ -87,30 +101,116 @@ default_row = 4
 default_col = 3
 
 # create a file
-file_name = xlsxwriter.Workbook("year schedule.xlsx")
-worksheet = file_name.add_worksheet("year schedule")
+file_name = xlsxwriter.Workbook(f"{year} schedule.xlsx")
+worksheet = file_name.add_worksheet(f"{year} schedule")
 
 # features for the cells
+
+# set a column's width
+worksheet.set_row(2, 8)  # 2
+# worksheet.set_row(1, 3)  # 2
+worksheet.set_column("A:A", 1)  # A
+worksheet.set_column("B:B", 5)  # B
+worksheet.set_column("C:C", 30) # C
+
 # months
-month_format1 = file_name.add_format({'bold': True, 'align': 'center', 'font_size':16, 'bg_color':'#E2FEC6', 'border':1, 'border_color':'black'})
+month_format1 = file_name.add_format({'bold': True, 'align': 'center', 'valign': 'vcenter', 'font_size':16, 'bg_color':'#EDEED6', 'border':1, 'border_color':'black'})
 month_format2 = file_name.add_format({'bold': True, 'align': 'center', 'font_size':16, 'bg_color':'#F1FFE7', 'border':1, 'border_color':'black'})
 # week_days_format
 week_days_format = file_name.add_format({'border':1, 'border_color':'black'})
+week_days_format_dayoff = file_name.add_format({'border':1, 'border_color':'black', 'bg_color':'#E6B8B7'})
+holiday_format = file_name.add_format({'border':1, 'border_color':'black', 'bg_color':'#EE7A6E'})
 # days_format
 days_format1 = file_name.add_format({'bg_color':'#F2F2F2', 'border':1, 'border_color':'black'})
-days_format2 = file_name.add_format({'bg_color':'#D9D9D9', 'border':1, 'border_color':'black'})
+days_format2 = file_name.add_format({'bg_color':'#E6E6E6', 'border':1, 'border_color':'black'})
+# employee_format
+employee_format1 = file_name.add_format({'border':1, 'border_color':'black', 'align': 'center'})
+employee_format = file_name.add_format({'border':1, 'border_color':'black'})
+# vacation_format
+vacation_format = file_name.add_format({'border':1, 'bg_color':'#B7DEE8', 'border_color':'black', 'align': 'center'})
+# empty_cell_format
+empty_cell_format = file_name.add_format({'border':1, 'border_color':'black'})
+# signature_format
+signature_format = file_name.add_format({'font_size': 9, 'color': 'blue', 'underline': True})
+
+# freeze the space
+worksheet.freeze_panes(6,3)
+
+# Company's logo
+worksheet.insert_image('B1', 'GST.png')
+
+for d in rrule(DAILY, dtstart=start_date, until=end_date):
+    worksheet.write(default_row, default_col, '', empty_cell_format)
+    worksheet.write(default_row + 1, default_col, '', empty_cell_format)
+    for line_number in range(len(vacation.keys())):  # number of employees
+        worksheet.write(default_row + line_number + 2, default_col, '', empty_cell_format)
+    default_col += 1
+
+default_row = 4
+default_col = 3
 
 # iterating over the dates
 for d in rrule(DAILY, dtstart=start_date, until=end_date):
+    # print(year_months[d.month], d.day, week_days[(d.weekday()+1)])
+    # print(d.day)
+    # print(d.date())
 
-    # create week_days
-    worksheet.write(default_row, default_col, week_days[(d.weekday()+1)], week_days_format)
+    for employee_num, employee__ in enumerate(vacation.keys()):
+        # print(employee__, vacation[employee__])
+        for vacation_num in vacation[employee__]:
+            # print(employee_num+1, employee__, vacation_num, vacation[employee__][vacation_num]) # vacation[employee__][vacation_num] - vacantion
+
+            # set variables to compare
+            first_vacation_day = int(vacation[employee__][vacation_num][0][:2])
+            first_vacation_month = int(vacation[employee__][vacation_num][0][3:5])
+            last_vacation_day = int(vacation[employee__][vacation_num][1][:2])
+            last_vacation_month = int(vacation[employee__][vacation_num][1][3:5])
+            vacation_duration = int(vacation[employee__][vacation_num][2])
+
+            # find days off number ('Sat' and 'Sun')
+            start = datetime(year, first_vacation_month, first_vacation_day)
+            end = datetime(year, last_vacation_month, last_vacation_day)
+            dayoff_number = 0
+            for day in rrule(DAILY, dtstart=start, until=end):
+                # print(year_months[d.month], d.day, week_days[(d.weekday() + 1)])
+                if week_days[(day.weekday() + 1)] == 'Sat' or week_days[(day.weekday() + 1)] == 'Sun':
+                    dayoff_number += 1
+                # add official holidays
+                if day.date() in listOfHolidays_date:
+                    dayoff_number += 1
+
+            # create a schedule
+            if d.day == first_vacation_day and d.month == first_vacation_month:
+               for day_ in range(vacation_duration + dayoff_number):
+                    worksheet.write(default_row + 1 + employee_num+1, default_col + day_, vacation_duration, vacation_format)
+
     # create days
     # set different color for the cells
     if d.month % 2 != 0:
         worksheet.write(default_row + 1, default_col, d.day, days_format1)
     if d.month % 2 == 0:
         worksheet.write(default_row + 1, default_col, d.day, days_format2)
+
+    # create week_days
+    if week_days[(d.weekday()+1)] == 'Sat' or week_days[(d.weekday()+1)] == 'Sun':
+        worksheet.write(default_row, default_col, week_days[(d.weekday() + 1)], week_days_format_dayoff)
+        worksheet.write(default_row+1, default_col, d.day, week_days_format_dayoff)
+        for line_number in range(len(vacation.keys())): # number of employees
+            worksheet.write(default_row + line_number+2, default_col, '', week_days_format_dayoff)
+    # add official holidays
+    elif d.date() in listOfHolidays_date:
+        worksheet.write(default_row, default_col, week_days[(d.weekday() + 1)], holiday_format)
+
+        worksheet.write(default_row + 1, default_col, d.day, holiday_format)
+        for line_number in range(len(vacation.keys())):  # number of employees
+            worksheet.write(default_row + line_number+2, default_col, '', holiday_format)
+    else:
+        worksheet.write(default_row, default_col, week_days[(d.weekday()+1)], week_days_format)
+
+    # add a holiday name
+    for dd in listOfHolidays:
+        if d.date() == dd[0]:
+            worksheet.write_comment(default_row, default_col, dd[1], {'visible': True, 'x_scale': 0.5, 'y_scale': 0.4, 'x_offset': 30, 'y_offset': -35})
 
     # create months and merge the cells
     if d.day == 1:
@@ -125,47 +225,70 @@ for d in rrule(DAILY, dtstart=start_date, until=end_date):
             worksheet.merge_range(default_row - 1, start_point, default_row - 1, default_col + end_point - 1,
                                       year_months[d.month], month_format1)
 
-
-    for employee_num, employee__ in enumerate(vacation.keys()):
-        # print(employee__, vacation[employee__])
-        for vacation_num in vacation[employee__]:
-
-            # set variables to compare
-            first_vacation_day = int(vacation[employee__][vacation_num][0][:2])
-            first_vacation_month = int(vacation[employee__][vacation_num][0][3:5])
-            last_vacation_day = int(vacation[employee__][vacation_num][1][:2])
-            last_vacation_month = int(vacation[employee__][vacation_num][1][3:5])
-            vacation_duration = int(vacation[employee__][vacation_num][2])
-
-            # find days off number ('Sat' and 'Sun')
-            start = datetime(year, first_vacation_month, first_vacation_day)
-            end = datetime(year, last_vacation_month, last_vacation_day)
-            dayoff_number = 0
-            for day in rrule(DAILY, dtstart=start, until=end):
-                if week_days[(day.weekday() + 1)] == 'Sat' or week_days[(day.weekday() + 1)] == 'Sun':
-                    dayoff_number += 1
-                # add official holidays
-                if day.date() in listOfHolidays_date:
-                    dayoff_number += 1
-
-            # create a schedule
-            if d.day == first_vacation_day and d.month == first_vacation_month:
-               for day_ in range(vacation_duration + dayoff_number):
-                    worksheet.write(default_row + 1 + employee_num+1, default_col + day_, vacation_duration)
-
     # go to the next day
     default_col += 1
 
-
 # create employees
+worksheet.merge_range(3, 1, 5, 2, "Employee", month_format1)
 default_row = 4
 default_col = 2
 for n_, employee_ in enumerate(vacation.keys()):
     # create an employee
-    worksheet.write(default_row + 2 + n_, default_col, employee_)
+    worksheet.write(default_row + 2 + n_, default_col, employee_, employee_format)
     # employee number
-    worksheet.write(default_row + 2 + n_, default_col - 1, n_+1)
+    worksheet.write(default_row + 2 + n_, default_col - 1, n_+1, employee_format1)
+
+# print(default_row + len(vacation.keys()))
+worksheet.write(default_row + len(vacation.keys())+3, default_col-1, "Created by @Victor Vetoshkin", signature_format)
+
+# file protecting option
+worksheet.protect(options={'autofilter': True})
 
 file_name.close()
 
 
+
+
+
+
+
+
+# print(datetime(2023,12,31))
+# print(calendar.monthrange(2023, 2)[1])
+
+# # iterating over the dates
+# for d in rrule(DAILY, dtstart=start_date, until=end_date):
+#     print(d.strftime("%Y-%m-%d"))
+
+
+# time = (datetime(2023,12,31) - datetime(2023,1,1)).days
+
+
+# for month in range(1,13):
+#     print(datetime(2023,12,31).weekday())
+
+
+#     worksheet.write(default_row + 1, default_col, day)
+# # print(datetime(day=vacation_day, month=vacation_month, year=year))
+# print(datetime(2023,1,1))
+# print(datetime(2023,12,31))
+# print(datetime(2023,12,31).day)
+# print(datetime(2023,12,31).weekday())
+# print((datetime(2023,12,31) - datetime(2023,1,1)).days)
+# print(timedelta(7).days)
+
+
+# print(int(f'{datetime.today():%j}'))
+
+
+# import holidays
+# import holidays.countries
+#
+# listOfHolidays = []
+# for ptr in holidays.RS(years=year).items():
+#     print(ptr)
+#     # print(ptr[0])
+#     # print(ptr[1])
+#     # listOfHolidays.append((ptr[0], ptr[1]))
+#
+# # print(listOfHolidays)
